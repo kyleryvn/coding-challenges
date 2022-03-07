2ad90818cf910e30b467a669433c7904c1e1a418
Kyle <78947025+kyleryvn@users.noreply.github.com>
2022-03-07 13:40:50 -0500

    Added setup.py

---
+++ challenges/python/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='coding-challenges',
+    version='1.0.0',
+    description='',
+    author='Kyle Schoenhardt',
+    url='https://github.com/kyleryvn/coding-challenges',
+    packages=find_packages(exclude=('tests', 'docs'))
+)
