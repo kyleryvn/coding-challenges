23f179076f86f72ae2aa6c810908a5465844c755
Kyle <78947025+kyleryvn@users.noreply.github.com>
2022-03-07 13:58:01 -0500

    Delete setup.py

--- challenges/python/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='coding-challenges',
-    version='1.0.0',
-    description='',
-    author='Kyle Schoenhardt',
-    url='https://github.com/kyleryvn/coding-challenges'
-)
