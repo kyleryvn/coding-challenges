7d0ccbfbbda3be9d3b35dfaef91ee6be16f99e96
Kyle <78947025+kyleryvn@users.noreply.github.com>
2022-03-07 12:34:20 -0500

    Added stocks_profit.py

---
+++ challenges/python/stocks_profit.py
@@ -0,0 +1,15 @@
+def find_pairs(target, stocks_profit):
+    pairs = []
+    result = ()
+
+    for i in range(len(stocks_profit)):
+        current = stocks_profit[i]
+        difference = target - current
+
+        if difference in stocks_profit:
+            if (difference, current) not in pairs and (current, difference) not in pairs:
+                pairs.append((difference, current))
+                result += 1
+
+    return result
+
